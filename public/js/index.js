54e5a04878e3a4a1e39c632b7dc72fc2a222f273
fede379 <fedeloe379@gmail.com>
2019-06-12 13:38:40 -0300

    added some controls

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,18 @@
-// TO-DO
-// Controls front
+$('#check').on('submit', function(event){    
+    if (isValidString($('#inputStr').val())) {
+        $("#error").text("")
+        $('#check').submit()
+    } else {
+        event.preventDefault()
+        $("#error").text("El string ingresado no es válido")
+    }
+})
+
+validate = () => {    
+    if (isValidString($('#inputStr').val())) $("#error").text("")
+    else $("#error").text("El string ingresado no es válido")
+}
+
+isValidString = (str) => {
+    return (/^[a-zA-Z]*$/).test(str)
+}
